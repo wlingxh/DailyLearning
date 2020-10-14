a955066c66c1a94cdb1a111f8d880b5b18a872bf
wlingxh <295355221@qq.com>
2020-10-14 15:14:24 +0800

    test git

--- demo-git/src/main/java/com/wwh/demo/git/Git.java
+++ demo-git/src/main/java/com/wwh/demo/git/Git.java
@@ -3,5 +3,6 @@ package com.wwh.demo.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Git Test");
+        System.out.println("Test Git");
     }
 }
